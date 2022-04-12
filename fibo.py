dd6f16d8ea72fb0d5eae14e815d515622433c4e3
aditya290919 <aditya.vig29@gmail.com>
2022-04-12 17:13:51 +0530

    fibonachi numbers

---
+++ fibo.py
@@ -0,0 +1,23 @@
+def fibo(n):
+
+    
+
+    a=0
+    b=1
+
+   
+    print(a)
+
+    print(b)
+
+    for i in range(2,n+1):
+
+        c=a+b
+
+        a=b
+        b=c
+
+        print(c)
+
+
+fibo(10)
