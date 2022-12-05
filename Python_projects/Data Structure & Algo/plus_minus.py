4ec4bf51f8c455e0a10f122f218b7241b2f77728
Aditya Vig <aditya.vig29@gmail.com>
2022-12-05 10:13:53 +0530

    plus minus

---
+++ Python_projects/Data Structure & Algo/plus_minus.py
@@ -0,0 +1,24 @@
+def plus_minus(arr):
+
+	positive = negative = zero = 0
+
+	for i in arr:
+
+		if i > 0:
+
+			positive+=1
+
+		if i < 0:
+
+			negative+=1
+
+		zero+=1
+
+
+	print(positive/len(arr))
+	print(negative/len(arr))
+	print(zero/len(arr))
+
+
+arr = [4,2,-5,0,1,-2]
+plus_minus(arr)
