2ae6243c2c0d6de6bdca66e5742b796dd2836606
Aditya Vig <aditya.vig29@gmail.com>
2023-01-04 21:28:47 +0530

    max sum of sub array

--- PYTHON/Data Structure & Algo/max_sum.py
+++ PYTHON/Data Structure & Algo/max_sum.py
@@ -8,3 +8,11 @@ def max_sum(arr,WindowSize):
 	window_sum = sum([arr[i] for i in range(WindowSize)])
 
 	max_sum = window_sum
+
+	for i in range(arraysize-WindowSize):
+		window_sum=WindowSize-arr[i]+arr[i+k]
+		max_sum=max(window_sum,max_sum)
+
+	return max_sum
+	
+
