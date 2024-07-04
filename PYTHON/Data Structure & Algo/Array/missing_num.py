872f207d2dc8b87ab6d56d55598a95bca58f7d99
Aditya Vig <aditya.vig29@gmail.com>
2024-07-04 12:32:45 +0300

    missing numbers

--- PYTHON/Data Structure & Algo/Array/missing_num.py
+++ PYTHON/Data Structure & Algo/Array/missing_num.py
@@ -1,6 +1,12 @@
+from typing import List
 class Solution:
 	def missingNumber(self,nums:List[int])->int:
 		current_sum=sum(nums)
 		n = len(nums)
 		intendedSum = n*(n+1)/2
 		return int(intendedSum - current_sum)
+
+
+s = Solution()
+answer=s.missingNumber([3,1,0])
+print(answer)
