32b1827e97c86ad46c22e83a8777d40f5a9bdfc3
Aditya Vig <aditya.vig29@gmail.com>
2025-12-26 19:49:28 +0530

    max profit

--- PYTHON/Data Structure & Algo/Array/best_time_to_buy_and_sell_stock.py
+++ PYTHON/Data Structure & Algo/Array/best_time_to_buy_and_sell_stock.py
@@ -1,13 +1,11 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        min_price = float('inf')
-        max_profit = 0
+def maxProfit(prices):
+    max_profit = 0
+    min_price = float('inf')
 
-        for price in prices:
-            if price<min_price:
-                min_price = price
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        else:
+            max_profit = max(max_profit, price - min_price)
 
-            elif price-min_price>max_profit:
-                max_profit = price - min_price
-
-        return max_profit
+    return max_profit
