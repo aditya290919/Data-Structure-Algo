e23caa095ecd1dd36a791ac72272c06b73f16e47
Aditya Vig <aditya.vig29@gmail.com>
2025-12-31 21:11:02 +0530

    two sum

--- PYTHON/Data Structure & Algo/Array/two_sum.py
+++ PYTHON/Data Structure & Algo/Array/two_sum.py
@@ -1,13 +1,7 @@
 def two_sum(nums, target):
-    index_of_num = {}
-
-    for i, num in enumerate(nums):
-        complement = target - num
-        if complement in index_of_num:
-            return [index_of_num[complement],i]
-
-
-
-        index_of_num[num] = i
-
-    return []
+    seen = {}
+    for i, x in enumerate(nums):
+        needed = target - x
+        if needed in seen:
+            return [nums[needed], i]
+        seen[x]  = i
